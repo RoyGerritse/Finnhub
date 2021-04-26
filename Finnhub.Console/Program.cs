323422ae9197d337b84c37ce8f6b69552c4aa4d7
Roy Gerritse <info@roygerritse.nl>
2021-04-26 16:46:19 +0200

    program

--- Finnhub.Console/Program.cs
+++ Finnhub.Console/Program.cs
@@ -1,13 +1,34 @@
-﻿using Finnhub.Client;
+﻿using System;
+using Finnhub.Client;
 
 namespace Finnhub.Console
 {
     public class Program
     {
+        private static EnvironmentSettings EnvironmentSettings { get; set; }
+
         public static void Main(string[] args)
         {
-            var client = new FinnhubClient("token");
+            EnvironmentSettings = new EnvironmentSettings();
+
+            var client = new FinnhubClient(EnvironmentSettings.Token);
             client.CryptoCandle("BINANCE:BTCUSDT", "D");
         }
+    }
+
+    public class EnvironmentSettings
+    {
+        private const string FinnhubToken = "FINNHUB_TOKEN";
+
+        public EnvironmentSettings()
+        {
+            var token = Environment.GetEnvironmentVariable(FinnhubToken);
+            if (token != null)
+            {
+                Token = token;
+            }
+        }
+
+        public string Token { get; }
     }
 }
